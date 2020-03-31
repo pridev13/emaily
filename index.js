0c329c532bf4bb283b680e4eaafba6c1bcd0bc06
pridev13 <m@mvhr.nl>
2020-03-31 15:38:53 +0200

    Set callback path for Passport

--- index.js
+++ index.js
@@ -10,14 +10,18 @@ passport.use(
 		clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-	}, (accessToken) => {
+	}, (accessToken, refreshToken, profile, done) => {
     console.log('Access token: ' + accessToken);
+    console.log('Refresh token: ' + refreshToken);
+    console.log('Profile: ' + profile);
   })
 );
 
 app.get('/auth/google', passport.authenticate('google', {
   scope: ['profile', 'email']
 }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
