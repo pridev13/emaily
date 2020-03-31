69e2138f98dd5dff741153699cfbd37137eac238
pridev13 <m@mvhr.nl>
2020-03-31 15:10:45 +0200

    Configuring PassportJS

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hey: 'buddy'});
-});
+passport.use(
+	new GoogleStrategy({
+		ClientID: keys.googleClientID,
+    ClientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+	}, (accessToken) => {
+    console.log('Access token: ' + accessToken);
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
