c6280c9ca2b17143ef92f8e8ba33b0412722bf5b
pridev13 <m@mvhr.nl>
2020-04-01 12:49:49 +0200

    Tweak Google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -20,10 +20,12 @@ passport.deserializeUser((id, done) => {
 });
 
 passport.use(
-	new GoogleStrategy(
-		{clientID: keys.googleClientID,
-		 clientSecret: keys.googleClientSecret,
-		 callbackURL: "/auth/google/callback"},
+	new GoogleStrategy({
+      clientID: keys.googleClientID,
+		  clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
 		(accessToken, refreshToken, profile, done) => {
 
       User
