3e61731143c69316ed9e8a88eda511abbdb614a5
pridev13 <m@mvhr.nl>
2020-04-01 16:12:55 +0200

    Added dummy components to routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware());
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose);
+
+const store = createStore(reducers, {}, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
 	<Provider store={store}>
