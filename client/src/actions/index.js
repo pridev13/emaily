00a4ff054ff0c97fdd37b603fbf7ea1d4d4dded4
pridev13 <m@mvhr.nl>
2020-04-01 17:35:09 +0200

    Added first action creator

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import {FETCH_USER} from './types';
+
+export const fetchUser = () => async (disp) => {
+	const res = await axios.get('/api/current_user');
+	disp({
+		type: FETCH_USER,
+		payload: res
+	});
+};
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 //custom
 import App from './components/App';
@@ -10,9 +11,16 @@ import reducers from './reducers';
 //CSS
 import 'materialize-css/dist/css/materialize.min.css';
 
-const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose);
+const composeEnhancers =
+	process.env.NODE_ENV === 'development'
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
 
-const store = createStore(reducers, {}, composeEnhancers(applyMiddleware()));
+const store = createStore(
+	reducers,
+	{},
+	composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
 	<Provider store={store}>
