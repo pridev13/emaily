4deafafc3c0de706f17ad8df92086613bd7c48cb
pridev13 <m@mvhr.nl>
2020-04-02 11:23:57 +0200

    Added Stripe Buy button to header

---
+++ client/src/components/Payments.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+	render() {
+		return (
+			<StripeCheckout
+				amount={500}
+				currency="EUR"
+				token={(token) => console.log(token)}
+				stripeKey={process.env.REACT_APP_STRIPE_KEY}
+			/>
+		);
+	}
+}
+
+export default Payments;
