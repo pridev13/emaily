f177eca0e063d7dfe16f7dbe95d10e8bbcdee041
pridev13 <m@mvhr.nl>
2020-04-02 09:46:38 +0200

    Made logo clickable

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 class Header extends Component {
 	renderContent = () => {
@@ -13,7 +14,11 @@ class Header extends Component {
 					</li>
 				);
 			default:
-				return <li><a href="/api/logout">Logout</a></li>;
+				return (
+					<li>
+						<a href="/api/logout">Logout</a>
+					</li>
+				);
 		}
 	};
 
@@ -21,9 +26,12 @@ class Header extends Component {
 		return (
 			<nav>
 				<div className="nav-wrapper">
-					<a href="#" className="brand-logo left">
+					<Link
+						to={this.props.auth ? '/surveys' : '/'}
+						className="brand-logo left"
+					>
 						Emaily
-					</a>
+					</Link>
 
 					<ul className="right">{this.renderContent()}</ul>
 				</div>
