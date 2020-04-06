807455e9285a385eaab317df4a99b9c2f630ed67
pridev13 <m@mvhr.nl>
2020-04-06 08:50:13 +0200

    Created mail template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const creditCheck = require('../middlewares/creditCheck');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, creditCheck, (req, res) => {
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thanks for voting');
+	});
+
+	app.post('/api/surveys', requireLogin, creditCheck, async (req, res) => {
 		const {title, body, subject, recipients} = req.body;
 
 		const survey = new Survey({
@@ -16,5 +22,26 @@ module.exports = (app) => {
 			_user: req.user.id,
 			dateSent: Date.now()
 		});
+
+		//create email
+		const mailer = new Mailer(survey, surveyTemplate(survey));
+
+		try {
+			//send email
+			await mailer.send();
+
+			//save survey
+			await survey.save();
+
+			//charge 1 credit
+			req.user.credits -= 1;
+
+			//save user
+			const user = await req.user.save();
+
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+		}
 	});
 };
